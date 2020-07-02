9f7bff797ff0dd9bf90358ebee290106a31b5a7c
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-02 18:59:35 +0500

    "Travello app added in project"

--- my_app/views.py
+++ my_app/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html', {'name':'Rustam'})
+    return render(request, 'home.html', {'name':'Rustam'})
+
+def add(request):
+    val1=request.POST["num1"]
+    val2=request.POST["num2"]
+    res=int(val1)+int(val2)
+    return render(request, 'result.html', {'result':res})
