0a2acec5436a728a1b907c0d55a50ffad0685eba
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-03 18:55:17 +0500

    Done with everything, pre step to UserRegistratoin

--- travello/models.py
+++ travello/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Destination(models.Model):
     name = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='media//pics')
+    img = models.ImageField(upload_to='cities/')
     desc =  models.TextField()
     price = models.IntegerField(default=0)
     offer = models.BooleanField(default=False)
