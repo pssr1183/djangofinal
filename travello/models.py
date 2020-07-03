1ce55f9d3862937afe2ce4d96dcb1248eccad3d0
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-03 09:44:42 +0500

    Pre step before creating DB tables, done 'Special Offer'

--- travello/models.py
+++ travello/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Destination:
-    id:int
-    name:str
-    img:str
-    desc:str
-    price:int
-    
+    id: int
+    name: str
+    img: str
+    desc: str
+    price: int
+    offer: bool
