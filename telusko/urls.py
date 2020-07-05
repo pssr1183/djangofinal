95d701d463177fecd3c34f200b8faf81100a4537
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-05 21:44:48 +0500

    Pre user registeration version

--- telusko/urls.py
+++ telusko/urls.py
@@ -19,8 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('travello.urls')),
     path('admin/', admin.site.urls),
+    path('', include('travello.urls')),
+    path('accounts/', include('accounts.urls')),
+
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
