16bf64700cb5f2111dfda256a1551abb07788b59
RΨST-4M <59933997+Rustam-Z@users.noreply.github.com>
2020-08-03 18:13:53 +0500

    Simple adding calculator

--- my_app/views.py
+++ my_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def home(request):
     return render(request, 'home.html', {'name':'Rustam'})
 
 def add(request):
+    # taking two numbers
     val1=request.POST["num1"]
     val2=request.POST["num2"]
+    # simple python calculations
     res=int(val1)+int(val2)
+    # rendering a web page
     return render(request, 'result.html', {'result':res})
